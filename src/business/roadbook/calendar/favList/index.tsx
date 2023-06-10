12edf328a34749e77e05d5ad631a67747e9cd780
zoka <465157222@qq.com>
2023-06-10 22:31:25 +0800

    添加预报天数控制，并优化查询过程

--- src/business/roadbook/calendar/favList/index.tsx
+++ src/business/roadbook/calendar/favList/index.tsx
@@ -10,6 +10,12 @@ interface IShowable {
 };
 
 export default function() {
+    enum E_fcDisplayMode {
+        DETAIL = 'detail',
+        SIMPLE = 'simple',
+        FIGURE = 'figure'
+    };
+
     let t0 = DayJS().startOf('day');
     let t1 = t0.add(DayJS.duration({ days: 1 }));
 
@@ -17,9 +23,21 @@ export default function() {
     let [listData, setListData] = useState<any[]>([]);
     let [spinning, setSpinning] = useState(false);
     let [fcLen, setFcLen] = useState(5);
+    let [fcDispMode, setFcDispMode] = useState(E_fcDisplayMode.DETAIL);
+    
 
+    /**
+     * 获取天气倾向分数
+     * @param fc 天气预报数组
+     * @returns 
+     */
     function getWeatherScore(fc: any[]) {
         let score = 0;
+
+        let tn = t0.add(DayJS.duration({ days: fcLen }));
+        fc = fc.filter(item => item.dt < tn.unix());
+
+        console.debug('参考预报：', tn.unix(), fc);
 
         fc.forEach((item: any) => {
             let iScore = 0;
@@ -82,27 +100,49 @@ export default function() {
             let {data} = await fetch.get('/api/roadPlan/favGeoLocation/list', {params: {page: 1, limit: 100}});
             setListData(data);
 
+            // 获取天气，但是不计算分数，也不变更排序（因为这样可能会导致重复请求）
             for (let i = 0; i < data.length; i++) {
                 let ow = new (Openweather as any)(data[i].lng, data[i].lat);
                 try {
                     let fc = await ow.getForecast();
                     let data2 = [...data];
                     data2[i].fc = fc;
-                    data2[i].score = getWeatherScore(fc);
                     data = data2;
-                    data.sort((a: any, b: any) => b.score - a.score);
                     setListData(data);
                 } catch (e: any) {
                     console.error(e);
                     message.error(e.message);
                 }
             }
+
+            sortItemsByScore(data);
+
         } catch (e: any) {
             console.error(e);
             message.error(e.message);
         } finally {
             setSpinning(false);
         }
+    }
+
+    function sortItemsByScore(data?: any[]) {
+        if (data === undefined) {
+            data = Array.from(listData);
+        }
+
+        console.debug('重新排序天气');
+
+        for (let i = 0; i < data.length; i++) {
+            let fc = data[i].fc;
+            if (fc instanceof Array) {
+                data[i].score = getWeatherScore(fc);
+            } else {
+                data[i].score = 114514; // 哼哼哼啊啊啊啊啊啊啊啊啊啊啊啊啊啊
+            }
+        }
+
+        let data2 = Array.from(data).sort((a: any, b: any) => b.score - a.score);
+        setListData(data2);
     }
 
     function showModal(ref?: MutableRefObject<IShowable | null>) {
@@ -177,22 +217,46 @@ export default function() {
         }
     }
 
+    function renderFcColumn() {
+        let fullComps = [
+            <Table.Column title={`今天 ${t0.format('YYYY-MM-DD')}`} width={'260px'} key="t0" render={renderForecast(0)}/>,
+            <Table.Column title={`明天 ${t1.format('YYYY-MM-DD')}`} width={'260px'} key="t1" render={renderForecast(1)}/>,
+            <Table.Column title={renderForecastTitle(2)}            width={'260px'} key="t2" render={renderForecast(2)}/>,
+            <Table.Column title={renderForecastTitle(3)}            width={'260px'} key="t3" render={renderForecast(3)}/>,
+            <Table.Column title={renderForecastTitle(4)}            width={'260px'} key="t4" render={renderForecast(4)}/>,
+        ];
+
+        return fullComps.slice(0, fcLen);
+    }
+
 
     useEffect(() => {
         onQuery();
     }, []);
+
+    useEffect(() => {
+        sortItemsByScore();
+    }, [fcLen]);
 
     return (
         <div className="f-fit-height f-flex-col">
             <div className="f-flex-two-side">
                 <Space>
                     <label>预报天数：</label>
-                    <Select value={fcLen} onChange={e => setFcLen(e)}>
+                    <Select value={fcLen} onChange={e => setFcLen(e)} style={{ width: '100px' }}>
                         { Array.from(
                             {length: 5}, 
                             (v, k: number) => <Select.Option value={k+1} key={k+1}>{k+1}</Select.Option>) 
                         }
                     </Select>
+
+                    <label>显示模式：</label>
+                    <Select value={fcDispMode} onChange={e => setFcDispMode(e)} style={{ width: '100px' }}>
+                        <Select.Option value={E_fcDisplayMode.DETAIL} key={E_fcDisplayMode.DETAIL}>详细文字</Select.Option>
+                        <Select.Option value={E_fcDisplayMode.SIMPLE} key={E_fcDisplayMode.SIMPLE}>简短文字</Select.Option>
+                        <Select.Option value={E_fcDisplayMode.FIGURE} key={E_fcDisplayMode.FIGURE}>图表</Select.Option>
+                    </Select>
+
                     <Button type="primary" loading={spinning} onClick={ () => onQuery() }>刷新</Button>
                 </Space>
                 <Space>
@@ -203,11 +267,7 @@ export default function() {
                 <Table dataSource={listData} size="small" 
                        scroll={{ x: 1300, y: 'calc(100vh - 270px)' }}>
                     <Table.Column title="地名" dataIndex="label" key="label" width={'180px'} fixed="left"/>
-                    <Table.Column title={`今天 ${t0.format('YYYY-MM-DD')}`} width={'260px'} key="t0" render={renderForecast(0)}/>
-                    <Table.Column title={`明天 ${t1.format('YYYY-MM-DD')}`} width={'260px'} key="t1" render={renderForecast(1)}/>
-                    <Table.Column title={renderForecastTitle(2)}            width={'260px'} key="t2" render={renderForecast(2)}/>
-                    <Table.Column title={renderForecastTitle(3)}            width={'260px'} key="t3" render={renderForecast(3)}/>
-                    <Table.Column title={renderForecastTitle(4)}            width={'260px'} key="t4" render={renderForecast(4)}/>
+                    {renderFcColumn()}
                     {/* <Table.Column title='偏好分数' dataIndex="score" key="score" width={100}/> */}
                     <Table.Column title="操作" render={(cell, row) => renderAction(row)} key="action" width={160} fixed="right"/>
                 </Table>
