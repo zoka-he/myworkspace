4e7849cd0a2fde5da786c0eaf6ca0191b31279a4
zoka_he <465157222@qq.com>
2023-07-20 16:58:26 +0800

    暂存修改

--- src/business/bike/bikeManage/Structures/index.tsx
+++ src/business/bike/bikeManage/Structures/index.tsx
@@ -1,12 +1,24 @@
 import { Button, Input, Space, Tree } from "antd";
+import { DlgHelper } from "../AppendStructureDlg";
 
-export default function() {
+interface StructuresProps {
+    dlgCtl: DlgHelper
+}
+
+export default function(props: StructuresProps) {
+
+    function onAddStructure() {
+        if (props.dlgCtl) {
+            props.dlgCtl.open();
+        }
+    }
+
     return (
         <div>
             <Space>
                 <label>搜索：</label>
                 <Input></Input>
-                <Button type={'primary'}>添加</Button>
+                <Button type={'primary'} onClick={onAddStructure}>添加</Button>
             </Space>
             { /** @ts-ignore */ }
             <Tree>
--- src/business/bike/bikeManage/WheelDev/index.tsx
+++ src/business/bike/bikeManage/WheelDev/index.tsx
@@ -1,18 +1,22 @@
 import {Table, Radio, Space} from 'antd';
 import Editor from '../../WheelDev';
+import { useState } from 'react';
 
-
-const { Column } = Table;
 
 export default function AccountManage() {
+    let [wheelFB, setWheelFB] = useState('front');
+
     return (
         <div>
-            <Space>
-                <Radio.Group>
-                    <Radio>前轮</Radio>
-                    <Radio>后轮</Radio>
-                </Radio.Group>
-            </Space>
+            <div>
+                <Space>
+                    <Radio.Group value={wheelFB} onChange={e => setWheelFB(e?.target?.value)}>
+                        <Radio value={'front'}>前轮</Radio>
+                        <Radio value={'back'}>后轮</Radio>
+                    </Radio.Group>
+                </Space>
+                <br />
+            </div>
             <Editor readonly={true}/>
         </div>
     )
