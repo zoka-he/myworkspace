fabba265d8e29faf92f066e1da40c209abee4052
zoka <465157222@qq.com>
2023-08-21 16:55:28 +0800

    fix：账户设定ui问题

--- src/business/user/profile/index.tsx
+++ src/business/user/profile/index.tsx
@@ -2,29 +2,38 @@ import { Tabs } from "antd";
 import Security from './security';
 import qs from 'querystring';
 import { useLocation } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Personalize from "./personalize";
 
 export default function() {
     let location = useLocation();
     let [tabKey, setTabKey] = useState('1');
 
-    let locationSearch = location.search || '';
-    if (locationSearch.startsWith('?')) {
-        locationSearch = locationSearch.substring(1);
-    }
-    let urlQuery = qs.parse(locationSearch);
+    // 初始化
+    useEffect(() => {
+        onLocationChange();
+    }, []);
 
-    if (urlQuery.tabKey && tabKey !== urlQuery.tabKey) {
-        if (urlQuery.tabKey instanceof Array) {
-            setTabKey(urlQuery.tabKey[0]);
-        } else {
-            setTabKey(urlQuery.tabKey);
+    // 路径变化时调用
+    useEffect(() => {
+        onLocationChange();
+    }, [location]);
+
+    function onLocationChange() {
+        let locationSearch = location.search || '';
+        if (locationSearch.startsWith('?')) {
+            locationSearch = locationSearch.substring(1);
         }
-    } else {
-        // setTabKey('1');
-    }
+        let urlQuery = qs.parse(locationSearch);
 
+        if (urlQuery.tabKey && tabKey !== urlQuery.tabKey) {
+            if (urlQuery.tabKey instanceof Array) {
+                setTabKey(urlQuery.tabKey[0]);
+            } else {
+                setTabKey(urlQuery.tabKey);
+            }
+        } 
+    }
 
     let items = [
         {
@@ -39,9 +48,13 @@ export default function() {
         },
     ];
 
+    function onActiveChange(e: any) {
+        setTabKey(e);
+    }
+
     return (
         <div className="f-fit-height f-flex-col">
-            <Tabs activeKey={tabKey} items={items}/>
+            <Tabs activeKey={tabKey} items={items} onChange={onActiveChange}/>
         </div>
     )
 }
