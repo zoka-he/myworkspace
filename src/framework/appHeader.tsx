6e38c8169931ff4a82006a37713c21f00aeae08f
zoka <465157222@qq.com>
2023-10-22 00:55:16 +0800

    logo下方预留菜单搜索

--- src/framework/appHeader.tsx
+++ src/framework/appHeader.tsx
@@ -1,17 +1,44 @@
 import { connect } from "react-redux"
 import { IRootState } from "../store"
+import { Input } from "antd"
+import { SearchOutlined } from '@ant-design/icons';
+import store from '@/src/store';
+import useDebounce from "../utils/hooks/useDebounce";
+import { setMenuSearchKey } from "@/src/store/navigatorSlice";
 
 const mapStateToProps = (state: IRootState) => {
     return {
         navMenu: state.navigatorSlice.navMenu,
+        searchKey: state.navigatorSlice.menuSearchKey,
         loginUser: state.loginSlice.user
     }
 }
 
-function AppHeader() {
+interface IAppHeaderProps {
+    searchKey: string
+}
+
+function AppHeader(props: IAppHeaderProps) {
+    function onSearch(value: string) {
+        store.dispatch(setMenuSearchKey(value));
+    }
+
+    let isShifterActive = !!props.searchKey;
+
     return (
-        <div className='f-fit-width f-align-center' style={{ backgroundColor: 'rgb(40 83 155)' }}>
-            <h2 style={{ color: 'white', lineHeight: '46px' }}>工作台</h2>
+        <div className='f-fit-width f-align-center m-app-header'>
+            <div className={`m-app-header_shifter${isShifterActive ? ' active' : ''}`}>
+                <h2 className="m-app-header_logo" style={{ color: 'white', lineHeight: '46px' }}>工作台</h2>
+                <div className={`m-app-header_research`}>
+                    <Input 
+                        value={props.searchKey}
+                        onChange={ e => onSearch((e.target as HTMLInputElement).value) }
+                        placeholder="在菜单中搜索..." 
+                        prefix={<SearchOutlined/>}
+                        allowClear
+                    />
+                </div>
+            </div>
         </div>
     )
 }
--- src/store/navigatorSlice.ts
+++ src/store/navigatorSlice.ts
@@ -1,28 +1,35 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 interface INavigatorState {
-    navMenu: any[]
+    navMenu: any[],
+    menuSearchKey: string
 }
 
 
 const navigatorSlice = createSlice({
     name: 'navigator',
     initialState: {
-        navMenu: []
+        navMenu: [],
+        menuSearchKey: ''
     },
     reducers: {
         setNavMenu: (state, { payload }) => {
             state.navMenu = payload;
+        },
+
+        setMenuSearchKey: (state, { payload }) => {
+            state.menuSearchKey = payload
         }
     }
 });
 
-const { setNavMenu } = navigatorSlice.actions;
+const { setNavMenu, setMenuSearchKey } = navigatorSlice.actions;
 
 export default navigatorSlice;
 export type {
     INavigatorState
 };
 export {
-    setNavMenu
+    setNavMenu,
+    setMenuSearchKey
 };
